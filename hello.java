c0afcad099957529e0ae09c4717cf69559b4c052
Darli <2910716852@qq.com>
2025-07-04 16:29:20 +0800

    添加了两个方法

--- hello.java
+++ hello.java
@@ -7,4 +7,10 @@ public class Test{
     public static int add(int a, int b) {
         return a + b;
     }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+    public static int mul(int a, int b) {
+        return a * b;
+    }
 }
